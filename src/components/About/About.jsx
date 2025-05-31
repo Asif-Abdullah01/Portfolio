ccbd97f29345c1c18b94dc8cd1b2caba1289d2b5
Asif-Abdullah01 <asifabdullah214365@gmail.com>
2025-05-31 14:05:42 +0600

    Photo Updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -60,7 +60,7 @@ const About = () => {
           >
             DOWNLOAD RESUME
           </a>
-          
+
         </div>
         {/* Right Side */}
         <div className="md:w-1/2 flex justify-center md:justify-end">
@@ -76,7 +76,7 @@ const About = () => {
             <img
               src={profileImage}
               alt="Asif Abdullah"
-              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+              className="w-full h-full rounded-full object-cover object-[center_10%] drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
             />
           </Tilt>
         </div>
